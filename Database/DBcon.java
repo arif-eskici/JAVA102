3c6014a2caa6f29b0a7fa864b5b1d8c498c9e174
Arif ESKİCİ <aaema4@gmail.com>
2022-09-29 10:43:48 +0300

    connect to postgreSQL

---
+++ Database/DBcon.java
@@ -0,0 +1,39 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBcon {
+    public static final String DB_URL = "jdbc:postgresql://localhost/dvdrental";
+    public static final String DB_USER = "postgres";
+    public static final String DB_PASSWORD = "12345";
+
+    public static void main(String[] args) throws SQLException {
+        Connection connect = null;
+        String stSql = "UPDATE film SET title = 'Ace naberr' WHERE film_id = 2";
+        String prSql = "UPDATE film SET title = ? WHERE film_id = ?";
+        String sql = "SELECT * FROM film";
+        try {
+            connect = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            //Statement st = connect.createStatement();
+            //st.executeQuery(stSql);
+
+            PreparedStatement pr = connect.prepareStatement(prSql);
+            pr.setInt(2, 2); 
+            pr.setString(1, "İnek ŞABAN");     
+            pr.executeUpdate();     
+
+            //st.close();
+            connect.close();
+            pr.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+}
---
+++ Threads/ThreadClass.java
@@ -0,0 +1,85 @@
+package Threads;
+
+import java.util.ArrayList;
+
+public class ThreadClass implements Runnable {
+
+    private int firstNumber = 1;
+
+    private final Object LOCK = new Object();
+
+    private ArrayList <Integer> list = new ArrayList<>();
+    private ArrayList <Integer> list2 = new ArrayList<>();
+    private ArrayList <Integer> list3 = new ArrayList<>();
+    private ArrayList <Integer> list4 = new ArrayList<>();
+    private ArrayList <Integer> oddNumbers = new ArrayList<>();
+    private ArrayList <Integer> evenNumbers = new ArrayList<>();
+
+    public ThreadClass () {}
+
+    @Override
+    public void run() {
+       
+        synchronized (LOCK) {
+            System.out.println(getFirstNumber());
+
+            if (this.firstNumber <= 2500) {
+                this.list.add(this.firstNumber);
+                System.out.println(this.firstNumber + " added to List1");
+            } else if (this.firstNumber > 2500 && this.firstNumber <= 5000) {
+                this.list2.add(this.firstNumber);
+                System.out.println(this.firstNumber + " added to List2");
+            } else if (this.firstNumber > 5000 && this.firstNumber <= 7500) {
+                this.list3.add(this.firstNumber);
+                System.out.println(this.firstNumber + " added to List3");
+            } else {
+                this.list4.add(this.firstNumber);
+                System.out.println(this.firstNumber + " added to List4");
+            }
+
+            if (this.firstNumber % 2 == 0) {
+                this.evenNumbers.add(this.firstNumber);
+                System.out.println(this.firstNumber + " added to Even Number List");
+            } else {
+                this.oddNumbers.add(this.firstNumber);
+                System.out.println(this.firstNumber + " added to Odd Number List");
+            }
+
+            this.firstNumber++;
+        }
+        
+    }
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public ArrayList<Integer> getList() {
+        return list;
+    }
+
+    public ArrayList<Integer> getList2() {
+        return list2;
+    }
+
+    public ArrayList<Integer> getList3() {
+        return list3;
+    }
+
+    public ArrayList<Integer> getList4() {
+        return list4;
+    }
+
+    public synchronized ArrayList<Integer> getOddNumbers() {
+        return oddNumbers;
+    }
+
+    public synchronized ArrayList<Integer> getEvenNumbers() {
+        return evenNumbers;
+    }
+
+}
